6469a4526139eed1c061e8b0fc0dfe4877a54460
Michał Kułaczkowski <m.kulaczkowski@gmail.com>
2020-02-11 01:11:48 +0100

    updated

--- nfcclient/__init__.py
+++ nfcclient/__init__.py
@@ -1,5 +1,10 @@
+from .config import ClientConfig
+from .nfc_reader import *
+
 import logging
 logging.getLogger(__name__).addHandler(logging.NullHandler())
 logging.getLogger(__name__).setLevel(logging.INFO)
+
+
 
 __version__ = "0.0.1"
