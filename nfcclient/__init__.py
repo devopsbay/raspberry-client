090c64110be0097ce664c7a8e46295d963a6d047
Michał Kułaczkowski <m.kulaczkowski@gmail.com>
2020-02-11 01:12:45 +0100

    added relative imports

--- nfcclient/__init__.py
+++ nfcclient/__init__.py
@@ -1,10 +1,5 @@
-from .config import ClientConfig
-from .nfc_reader import *
-
 import logging
 logging.getLogger(__name__).addHandler(logging.NullHandler())
 logging.getLogger(__name__).setLevel(logging.INFO)
-
-
 
 __version__ = "0.0.1"
