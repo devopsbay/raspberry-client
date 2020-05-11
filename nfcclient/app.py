33ac6ee11f3ad17ff99cb843c7df62f06ea7bfd8
Michał Kułaczkowski <m.kulaczkowski@gmail.com>
2020-05-11 12:05:23 +0200

    changed modulo to bigger

--- nfcclient/app.py
+++ nfcclient/app.py
@@ -71,9 +71,10 @@ def client_app():
         for reader in readers:
             read_from_card(reader, client_config)
         loop_counter += 1
-        if loop_counter % 100:
+        if loop_counter > 20:
             logging.info('Re-init Readers')
             readers = init_readers(client_config)
+            loop_counter = 0
 
 
 def auth_api_call(client_config, card_id, door):
