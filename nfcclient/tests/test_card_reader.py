2cbdc217c2dc78c426d217d8db0fc598a66a54f5
mistillate <tchalupnik@gmail.com>
2020-06-19 17:04:26 +0200

    Remove calendar tests leftovers

--- nfcclient/tests/test_card_reader.py
+++ nfcclient/tests/test_card_reader.py
@@ -5,44 +5,32 @@ from nfcclient.nfc_reader.nfc_reader_mock import NFCReaderMock
 
 
 @pytest.mark.asyncio
-def test_authorize_by_card(event_loop, requests_mock, caplog, mocker, config):
+def test_authorize_by_card(event_loop, requests_mock, caplog, config):
     card_id = "1"
     door_name = "103"
     requests_mock.get(f"http://localhost:8123/auth/card/1/103", json={"status": True})
-    mocker.patch("nfcclient.calendar.Calendar.is_operating", return_value=True)
 
     assert event_loop.run_until_complete(authorize(config=config, card_id=card_id, door_name=door_name)) is True
     assert f"{card_id} Used" in caplog.text
 
 
 @pytest.mark.asyncio
-def test_authorize_by_master_card(event_loop, caplog, mocker, config):
+def test_authorize_by_master_card(event_loop, caplog, config):
     card_id = "0xda0x130x640x1a"
     door_name = "103"
-    mocker.patch("nfcclient.calendar.Calendar.is_operating", return_value=True)
 
     assert event_loop.run_until_complete(authorize(config=config, card_id=card_id, door_name=door_name)) is True
     assert f"Master Card {card_id} Used" in caplog.text
 
 
 @pytest.mark.asyncio
-def test_not_authorized_by_card(event_loop, requests_mock, mocker, caplog, config):
+def test_not_authorized_by_card(event_loop, requests_mock, caplog, config):
     card_id = "1"
     door_name = "103"
     requests_mock.get(f"http://localhost:8123/auth/card/{card_id}/{door_name}", json={"status": False})
-    mocker.patch("nfcclient.calendar.Calendar.is_operating", return_value=True)
 
     assert event_loop.run_until_complete(authorize(config=config, card_id=card_id, door_name=door_name)) is False
     assert f"Unauthorized Card {card_id}" in caplog.text
-
-
-@pytest.mark.asyncio
-def test_not_authorized_by_calendar(event_loop, mocker, config):
-    card_id = "1"
-    door_name = "103"
-    mocker.patch("nfcclient.calendar.Calendar.is_operating", return_value=False)
-
-    assert event_loop.run_until_complete(authorize(config=config, card_id=card_id, door_name=door_name)) is False
 
 
 @pytest.mark.asyncio
