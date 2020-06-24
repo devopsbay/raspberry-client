e491a3ea2367b35213621b675fe76cadc680ba7f
mistillate <tchalupnik@gmail.com>
2020-06-24 17:25:33 +0200

    Update config test server

--- config_test_server.py
+++ config_test_server.py
@@ -6,13 +6,26 @@ routes = web.RouteTableDef()
 @routes.get("/config/1")
 async def config(request):
     return web.json_response({
-        "master_keys": ["0x2b0x150x270xc", "0xda0x130x640x1a", "0xca0xbf0x570x1a", "0xa0x720xa90x15"],
+        "master_keys": ["0x2b0x150x270xc"],
         "doors": [
-            {"name": "103", "pin_id": 21, "readers": ["D23", "D24"]},
-            {"name": "101", "pin_id": 20, "readers": ["D22", "D27"]},
+            {"name": "103", "pin_id": 21, "readers": ["D8"]},
         ],
-        "door_open_seconds": 1,
+        "door_open_seconds": 5,
     })
+
+
+@routes.get("/auth/card/{card_id}/{door_name}")
+async def config(request):
+    if request.match_info["card_id"] == "0x170xff0x7a0x4b":
+        return web.json_response({
+            "username": "Jan Kowalski",
+            "expiration": "2000-12-12",
+            "status": True,
+        })
+    else:
+        return web.json_response({
+            "status": False,
+        })
 
 
 app = web.Application()
