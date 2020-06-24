abc4f9a4455ee3dc81b5ec72767858cf112bc10a
mistillate <tchalupnik@gmail.com>
2020-06-24 12:38:38 +0200

    Remove index

--- nfcclient/api.py
+++ nfcclient/api.py
@@ -1,6 +1,5 @@
 import asyncio
 
-import aiohttp_jinja2
 from aiohttp import web
 
 from nfcclient.gpio_client import gpio_client
@@ -16,8 +15,3 @@ async def open_door(request):
     seconds = int(request.rel_url.query.getone("seconds", gpio_client.door_open_seconds))
     asyncio.create_task(gpio_client.open_door(door_name=door_name, seconds=seconds))
     return web.json_response({"status": "ok"})
-
-
-@aiohttp_jinja2.template("index.jinja2")
-async def index(request):
-    return {}
