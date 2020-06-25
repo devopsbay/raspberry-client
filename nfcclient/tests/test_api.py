16ee082698440c08fd4215c4272406d093f98ed3
mistillate <tchalupnik@gmail.com>
2020-06-25 14:21:16 +0200

    Update test

--- nfcclient/tests/test_api.py
+++ nfcclient/tests/test_api.py
@@ -1,7 +1,5 @@
 import pytest
 
-from nfcclient.config import ClientConfig
-
 pytestmark = pytest.mark.asyncio
 
 
@@ -13,9 +11,8 @@ async def test_api_refresh(cli, aiohttp_app, monkeypatch, requests_mock):
         "[{\"name\":\"103\",\"pin_id\":21,\"readers\":[\"D23\",\"D24\"]}]",
     )
     monkeypatch.setenv("DOOR_OPEN_SECONDS", "1")
-    config = ClientConfig.from_env()
 
-    assert config.doors[0].name == "103"
+    assert aiohttp_app["config"].doors[0].name == "103"
     requests_mock.get("http://localhost:8123/config/1123", json={
         "master_keys": ["0x2b0x150x270xc", "0xda0x130x640x1a", "0xca0xbf0x570x1a", "0xa0x720xa90x15"],
         "doors": [
@@ -25,10 +22,9 @@ async def test_api_refresh(cli, aiohttp_app, monkeypatch, requests_mock):
         "door_open_seconds": 1,
     })
 
-    aiohttp_app["config"] = config
     response = await cli.get("/refresh/")
     assert response.status == 200
-    assert config.doors[0].name == "100"
+    assert aiohttp_app["config"].doors[0].name == "100"
 
 
 async def test_api_door_open(cli, aiohttp_app, mocker, gpio, door_manager):
