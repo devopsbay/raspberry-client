c13627f52e09f09875ef82e63a6e6113471cf03d
Michał Kułaczkowski <m.kulaczkowski@gmail.com>
2021-12-09 05:01:14 +0100

    updated cors settings

--- nfcclient/http_server.py
+++ nfcclient/http_server.py
@@ -12,14 +12,18 @@ async def aiohttp_server(app, host: str, port: int):
 
 
 app = web.Application()
+for route in routes:
+    app.router.add_route(route[0], route[1], route[2], name=route[3])
+
+# Configure default CORS settings.
 cors = aiohttp_cors.setup(app, defaults={
     "*": aiohttp_cors.ResourceOptions(
             allow_credentials=True,
             expose_headers="*",
             allow_headers="*",
-    )})
+        )
+})
 
-for route in routes:
-    #app.router.add_route(route[0], route[1], route[2], name=route[3])
-    resource = cors.add(app.router.add_resource(route[1]))
-    route = cors.add(resource.add_route(route[0], route[2]))
+# Configure CORS on all routes.
+for route in list(app.router.routes()):
+    cors.add(route)
