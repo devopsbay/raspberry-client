aa438f308d27353ed1f4b00410c47a80997b69d7
Michał Kułaczkowski <m.kulaczkowski@gmail.com>
2021-12-09 04:46:59 +0100

    updated requirements and added CORS to the http_server

--- nfcclient/http_server.py
+++ nfcclient/http_server.py
@@ -1,4 +1,5 @@
 from aiohttp import web
+import aiohttp_cors
 
 from nfcclient.router import routes
 
@@ -11,5 +12,12 @@ async def aiohttp_server(app, host: str, port: int):
 
 
 app = web.Application()
+cors = aiohttp_cors.setup(app, defaults={
+        # Allow all to read all CORS-enabled resources from
+        "http://localhost:3001": aiohttp_cors.ResourceOptions(),
+    })
+
 for route in routes:
-    app.router.add_route(route[0], route[1], route[2], name=route[3])
+    #app.router.add_route(route[0], route[1], route[2], name=route[3])
+    resource = cors.add(app.router.add_resource(route[1]))
+    route = cors.add(resource.add_route(route[0], route[2])
